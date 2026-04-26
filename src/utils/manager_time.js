381ce150a2ffbb5ccebb1f266d21bdf453ae4735
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-04-26 23:46:13 +0600

    timer manager added

---
+++ src/utils/manager_time.js
@@ -0,0 +1,16 @@
+const getExpirationDate = (durationStr) => {
+  const match = durationStr.match(/^(\d+)([smhd])$/);
+  const value = parseInt(match[1]);
+  const unit = match[2];
+
+  const multipliers = {
+    s: 1000,
+    m: 60 * 1000,
+    h: 60 * 60 * 1000,
+    d: 24 * 60 * 60 * 1000
+  };
+
+  return new Date(Date.now() + value * multipliers[unit]);
+};
+
+export default getExpirationDate;
