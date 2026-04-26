88643ba9f3099bdc02a21eba7469fb1137287389
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-04-26 12:39:20 +0600

    error manager added

---
+++ src/utils/manager_error.js
@@ -0,0 +1,37 @@
+import validatorLoginUser from "../validator/validator_login.js";
+import validatorRegistration from "../validator/validator_registration.js";
+
+const managerError = (object, key) => {
+    let currentError;
+    switch (key) {
+        case "registration":
+            {
+                const { error } = validatorRegistration.validate(object);
+                currentError = error;
+                break;
+            }
+
+        case "login":
+            {
+                const { error } = validatorLoginUser.validate(object);
+                currentError = error;
+                break;
+            }
+
+    }
+    if (currentError) {
+        const userError = currentError.details;
+        userError.statusCode = 400;
+        throw userError;
+    }
+}
+
+const generateError = (message, statusCode) => {
+    const error = new Error(message);
+    const userError = [error];
+    userError.statusCode = statusCode;
+    // console.log("generate error", userError);
+    throw userError;
+}
+
+export { managerError, generateError };
