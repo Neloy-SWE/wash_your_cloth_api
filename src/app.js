6d5e91b5f121889520adeb0cb9a64a199799e45a
Neloy-SWE <taufiqneloy.swe@gmail.com>
2026-04-29 13:07:16 +0600

    rate limiter added globally

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import routerAuth from "./feature/auth/route_auth.js";
 import dotenv from 'dotenv';
 import dotenvExpand from 'dotenv-expand';
 import db from "./model/index_model.js";
+import rateLimit from "express-rate-limit";
 
 const myEnv = dotenv.config();
 dotenvExpand.expand(myEnv);
@@ -18,6 +19,17 @@ app.use((req, res, next) => {
     next();
 });
 
+const globalLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    limit: 5,
+    handler: (req, res, next) => {
+        res.status(429).json({ status: "limitExceeded", message: "Too many requests" });
+    }
+});
+
+// apply rate limiter to global level:
+app.use(globalLimiter);
+
 // application check
 app.get("/", (req, res) => {
     res.send("Welcome to Wash Your Cloth API");
@@ -30,12 +42,12 @@ app.use("/auth", routerAuth);
 app.use((error, req, res, next) => {
     // console.error("global error handler: ", error);
     res.status(error.statusCode || 500)
-    .json({ error: error.errors?.map(e => e.message) || error?.map(e => e.message) || "Internal Server Error" });
+        .json({ error: error.errors?.map(e => e.message) || error?.map(e => e.message) || "Internal Server Error" });
     // .json({ error: error.errors?.map(e => e.message) || "Internal Server Error" });
 });
 
 db.sequelize.sync({ alter: true }).then(() => {
-// db.sequelize.sync().then(() => {
+    // db.sequelize.sync().then(() => {
     console.log("Database synchronized");
     const PORT = process.env.PORT;
     app.listen(PORT, () => {
